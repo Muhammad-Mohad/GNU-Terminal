50ffd0fb935802e75097d53abb631e1d834b1364
Muhammad Mohad <l230784@lhr.nu.edu.pk>
2024-10-07 23:39:21 +0500

    ok

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "source/commands.cpp"
 using namespace std;
 
 int main()
 {
-    
+    string command;
+    Window();
+    while(true)
+    {
+        cout << endl << "Welcome to Linux that is made in C++" << endl << endl;
+        cout << "Enter Command: ";
+        getline(cin, command);
+        ExecuteCommand(command);
+    }
 
     return 0;
 }
---
+++ source/commands.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+void Window()
+{
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+void ExecuteCommand(string& command)
+{
+    if(command == "exit")
+    {
+        cout << "Exiting Terminal..." << endl;
+        exit(EXIT_SUCCESS);
+    }
+    else if(command == "clear")
+        Window();
+    else
+    {
+        cout << "Invalid Command" << endl;
+        system("pause");
+    }
+    // Window();
+}
